f6b45385dec90a13c26d539bf0e257cf78577f4d
Ahmad <ahmad.bitar@nordsec.com>
2024-09-19 23:10:08 +0300

    feat/init-astro-storyblok-project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,32 @@
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import tailwind from '@astrojs/tailwind';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+        config: 'storyblok/Config',
+        hero: 'components/Hero',
+        'popular-articles': 'storyblok/PopularArticles',
+        'all-articles': 'storyblok/AllArticles',
+        article: 'storyblok/Article',
+      },
+    }),
+    tailwind(),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+});
